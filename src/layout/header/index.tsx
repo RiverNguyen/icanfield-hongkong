530a49d58d1627501a1014024446062c30cea389
SieuBuong <huycloud1999@gmail.com>
2024-12-19 16:23:43 +0700

    feat: add logo to header and new SVG icon for homepage

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import ImageV2 from '@/components/image/ImageV2'
 const Header = () => {
   return <header>
-    <div className='header-top'>
-      top
+    <div className='header-top section-container bg-[linear-gradient(118deg,#2E1506_69.75%,#95502F_142.7%,#F5C178_182.76%)] flex items-center justify-between'>
+      <div>
+        <ImageV2 src='/icons' alt='logo' width={100} height={100} />
+      </div>
+      <div></div>
     </div>
     <div className='header-bottom'>
       bottom
