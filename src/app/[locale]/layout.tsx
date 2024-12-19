ef97dbe6b9d8f3249b1192941059cfc5d01938f3
Jhuy02 <ttruonghuy2502@gmail.com>
2024-12-19 15:54:20 +0700

    update: color global

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type {Metadata} from 'next'
-import localFont from 'next/font/local'
 import '../globals.css'
 //
 import {NextIntlClientProvider} from 'next-intl'
