a676a2ae885cf24cdd5bb76a8bdd02e71a0cbcc8
SieuBuong <huycloud1999@gmail.com>
2024-12-20 16:43:56 +0700

    feat: add brown color to Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,7 @@ const config: Config = {
         textwhitetest: '#f7f6f1',
         background: '#F6F6F4',
         'Phase-1-Brown': '#5C321E',
+        brown: '#5C321E',
         greentext: '#254432',
         bodytext: '#5C5C5C',
         tagtext: '#A39A9A',
