9f91007c3cb49baad9143d74a8bf95393791900a
SieuBuong <huycloud1999@gmail.com>
2024-12-25 11:12:41 +0700

    feat: implement MapDiscover component and update imports in About Us and HomePage

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -1,6 +1,6 @@
 import AwardsCertifications from '@/sections/aboutus/awards-certifications'
 import awardsCertifications from '@/sections/aboutus/awards-certifications/constants'
-import BannerAboutus from '@/sections/aboutus/banner'
+import BannerAboutus from '@/sections/aboutus/banner/BannerAboutus'
 import Mission from '@/sections/aboutus/banner/Mission'
 
 const IndexAboutUs = () => {
