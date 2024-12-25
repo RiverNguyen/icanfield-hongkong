867e88b3bfabc41f91c31acb293aa13ec9a2c3ff
Jhuy02 <ttruonghuy2502@gmail.com>
2024-12-25 09:01:26 +0700

    section2 pioneering-mission

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -1,10 +1,12 @@
 import BannerAboutus from "@/sections/aboutus/banner/BannerAboutus";
 import Mission from "@/sections/aboutus/banner/Mission";
+import PioneeringMission from "@/sections/aboutus/pioneering-mission/PioneeringMission";
 
 const IndexAboutUs = () => {
     return <>
         <BannerAboutus />
         <Mission />
+        <PioneeringMission />
     </>;
 };
 
--- src/sections/aboutus/banner/ImageBannerMb.tsx
+++ src/sections/aboutus/banner/ImageBannerMb.tsx
@@ -93,8 +93,8 @@ export default function ImageBannerMb({className}: {className?: string}) {
     <clipPath id="clip0_1763_15867">
     <rect width="375" height="812" fill="white"/>
     </clipPath>
-    <image id="image0_1763_15867" width="3936" height="2624" xlinkHref="/imgs/about-us/image-bg.webp"/>
-    <image id="image1_1763_15867" width="4096" height="2731" xlinkHref="/imgs/about-us/d-city.webp"/>
+    <image id="image0_1763_15867" width="3936" height="2624" xlinkHref="/imgs/about-us/banner/image-bg.webp"/>
+    <image id="image1_1763_15867" width="4096" height="2731" xlinkHref="/imgs/about-us/banner/d-city.webp"/>
     </defs>
     </svg>
     
