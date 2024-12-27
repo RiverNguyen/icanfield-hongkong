abcc486833bd1082057204838ef083ac3e2c9c8d
FinnTVD <finnit.th@gmail.com>
2024-12-27 14:38:52 +0700

    UI: gird posts, tab, search and filter

--- src/components/banner-static/index.tsx
+++ src/components/banner-static/index.tsx
@@ -19,23 +19,25 @@ export const BannerStatic: FC<IBannerStaticProps> = ({
   children,
 }) => {
   return (
-    <section className='relative min-h-[36.45rem] pt-[6.44rem]'>
+    <section className='relative min-h-[28.75rem] overflow-hidden pt-[3.75rem] sm:min-h-[36.45rem] sm:pt-[6.44rem]'>
       <ImageV2
         src={backgroundImage.src}
         alt={backgroundImage.alt}
         width={1600 * 2}
         height={478 * 2}
-        className='absolute bottom-0 left-0 right-0 top-[6.44rem] h-full w-full object-cover'
+        className='absolute bottom-0 left-0 right-0 top-[3.75rem] h-full w-full object-cover sm:top-[6.44rem]'
       />
-      <div className='absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-black/30'></div>
-      <div className='absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-[linear-gradient(26deg,rgba(0,0,0,0.70)_-19.37%,rgba(0,0,0,0.00)_128.25%)]'></div>
+      <div className='absolute bottom-0 left-0 right-0 top-0 bg-black/30'></div>
+      <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(26deg,rgba(0,0,0,0.70)_-19.37%,rgba(0,0,0,0.00)_128.25%)]'></div>
       <div
         className={cn(
           'z-1 relative mx-auto mt-[5.81rem] flex max-w-full flex-col px-[1rem] sm:max-w-[90rem] sm:flex-row sm:flex-wrap sm:px-0',
           {'mt-[1.5rem]': !!children},
         )}
       >
-        <BannerLine className='mb-[1rem] max-w-full sm:max-w-[58.96875rem] xsm:order-2' />
+        {titleTop && titleBottom && description && (
+          <BannerLine className='mb-[1rem] mt-[1rem] max-w-full sm:mt-0 sm:max-w-[58.96875rem] xsm:order-2' />
+        )}
         <div className='sm:mr-[5rem] xsm:order-1'>
           <h1 className='font-optima text-[2.375rem] font-medium leading-[1.2] tracking-[-0.0475rem] text-white sm:text-[5rem] sm:tracking-[-0.1rem]'>
             {titleTop}
@@ -44,7 +46,7 @@ export const BannerStatic: FC<IBannerStaticProps> = ({
             {titleBottom}
           </h2>
         </div>
-        <p className='max-w-[20.2rem] text-[0.75rem] font-semibold uppercase leading-[1.5] text-white/85 sm:text-[1rem] xsm:order-3'>
+        <p className='text-[0.75rem] font-semibold uppercase leading-[1.5] text-white/85 sm:max-w-[20.2rem] sm:text-[1rem] xsm:order-3'>
           {description}
         </p>
       </div>
@@ -54,39 +56,11 @@ export const BannerStatic: FC<IBannerStaticProps> = ({
 
 function BannerLine({className}: {className?: string}) {
   return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={947}
-      height={6}
-      viewBox='0 0 947 6'
-      fill='none'
-      className={className}
-    >
-      <path
-        d='M0.333333 3C0.333333 4.47276 1.52724 5.66667 3 5.66667C4.47276 5.66667 5.66667 4.47276 5.66667 3C5.66667 1.52724 4.47276 0.333333 3 0.333333C1.52724 0.333333 0.333333 1.52724 0.333333 3ZM3 3.5H946.5V2.5H3V3.5Z'
-        fill='url(#paint0_linear_1889_53326)'
-        fillOpacity={0.3}
-      />
-      <defs>
-        <linearGradient
-          id='paint0_linear_1889_53326'
-          x1={3}
-          y1={3.5}
-          x2={946.5}
-          y2={3.5}
-          gradientUnits='userSpaceOnUse'
-        >
-          <stop
-            offset={0.5}
-            stopColor='white'
-          />
-          <stop
-            offset={1}
-            stopColor='white'
-            stopOpacity={0}
-          />
-        </linearGradient>
-      </defs>
-    </svg>
+    <div
+      className={cn(
+        'relative h-[1px] w-full max-w-[58.96875rem] bg-[linear-gradient(to_right,rgba(255,255,255,1)_50%,rgba(255,255,255,0)_100%)] opacity-60 after:absolute after:left-0 after:top-1/2 after:block after:h-[0.4rem] after:w-[0.4rem] after:-translate-y-1/2 after:rounded-full after:bg-white after:content-[""]',
+        className,
+      )}
+    ></div>
   )
 }
--- src/pages/page-blogs/index.tsx
+++ src/pages/page-blogs/index.tsx
@@ -1,17 +1,22 @@
 import {BannerStatic} from '@/components/banner-static'
 import blogBanner from '@/sections/blogs/banner/constants'
 import WrapperConnectUs from '@/sections/blogs/connect-us/WrapperConnectUs'
+import {FeaturedNews} from '@/sections/blogs/featured-news'
+import featuredNews from '@/sections/blogs/featured-news/constants'
 import ListBlogs from '@/sections/blogs/list-blogs'
 import {FC} from 'react'
 
-interface IPageBlogsProps {}
+interface IPageBlogsProps {
+  test: string
+}
 
 export const PageBlogs: FC<IPageBlogsProps> = ({}) => {
   return (
     <>
       <BannerStatic {...blogBanner}></BannerStatic>
-      <WrapperConnectUs />
       <ListBlogs />
+      <WrapperConnectUs />
+      <FeaturedNews {...featuredNews} />
     </>
   )
 }
