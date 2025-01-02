adb3b4c51d6dbbff7be8bd3ed0309fc5045adf3a
leetaam.lt <leetaam.lt@gmail.com>
2025-01-02 14:58:14 +0700

    BE/Homepage

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -9,6 +9,11 @@ const endpoints = {
     list: '/categories',
   },
   homepage: '/pages/96',
+  homeFeatured: '/home-featured',
+  contactForm: {
+    id: '317',
+    unitTag: 'd349187',
+  },
 }
 
 export default endpoints
