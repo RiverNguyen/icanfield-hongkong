530f5298a65e63d48bcf12ca2365ca4cbc77d07c
Jhuy02 <ttruonghuy2502@gmail.com>
2025-01-03 14:09:47 +0700

    [code]: thư mục

--- src/sections/immigration/banner/BannerImmigration.tsx
+++ src/sections/immigration/banner/BannerImmigration.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import {Breadcrumb} from '@/components/breadcrumb'
 import ImageV2 from '@/components/image/ImageV2'
 import useIsMobile from '@/hooks/useIsMobile'
@@ -8,27 +9,36 @@ export default function BannerImmigration() {
   const isMobile = useIsMobile()
   return (
     <section className='relative h-[58.125rem] w-full sm:mt-[6.4375rem] xsm:h-[50.75rem]'>
-      <div className='absolute left-[5rem] top-0 z-20 space-y-[0.875rem]'>
-        <Breadcrumb
-          items={[
-            {label: 'Home', href: '/'},
-            {label: 'Blogs', href: '#'},
-          ]}
-        />
-        <div className='section-container space-y-[0.25rem]'>
-          <h1 className='background_clip--text bg-[linear-gradient(98deg,#95502F_41.26%,#F5C178_97.06%)] font-optima text-[5rem] font-medium leading-[1.2] tracking-[-0.1rem]'>
+      <div className='absolute left-[5rem] top-0 z-20 space-y-[0.875rem] xsm:left-0 xsm:top-[calc(3.75rem+4.69rem)]'>
+        {!isMobile && (
+          <Breadcrumb
+            items={[
+              {label: 'Home', href: '/'},
+              {label: 'Blogs', href: '#'},
+            ]}
+          />
+        )}
+        <div className='space-y-[0.25rem] section-container xsm:px-[0.5rem]'>
+          <h1 className='background_clip--text bg-[linear-gradient(98deg,#95502F_41.26%,#F5C178_97.06%)] font-optima text-[5rem] font-medium leading-[1.2] tracking-[-0.1rem] xsm:text-[2.5rem] xsm:leading-[1.3] xsm:tracking-[-0.05rem]'>
             ĐỊNH CƯ CANADA
           </h1>
-          <p className='hero-title text-[#474736]'>Khởi Đầu Tương Lai Mới</p>
+          <p className='text-[#474736] hero-title'>Khởi Đầu Tương Lai Mới</p>
         </div>
       </div>
       {isMobile ? (
-        <div className='relative size-full sm:hidden'></div>
+        <div className='relative size-full sm:hidden'>
+          <ImageV2
+            className='size-full'
+            fill
+            alt=''
+            src={'/imgs/immigration/d-hero_bannerMB.webp'}
+          />
+        </div>
       ) : (
         <div className='relative size-full xsm:hidden'>
           <ImageV2
             className='absolute z-20 size-full'
-            src={'/imgs/immigration/d-main_content_container.webp'}
+            src={'/imgs/immigration/d-main_content_containerV2.webp'}
             alt=''
             width={1600}
             height={820}
