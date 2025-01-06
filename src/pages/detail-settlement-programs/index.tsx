6efadd2dc68ca61460d61f91a058e0a35593dc24
leetaam.lt <leetaam.lt@gmail.com>
2025-01-06 16:46:42 +0700

    settlement-programs/section-program-benefits

--- src/pages/detail-settlement-programs/index.tsx
+++ src/pages/detail-settlement-programs/index.tsx
@@ -1,5 +1,7 @@
 import {Banner} from '@/sections/detail-settlement-programs/banner'
 import bannerSettlementPrograms from '@/sections/detail-settlement-programs/banner/constants'
+import {ProgramBenefits} from '@/sections/detail-settlement-programs/program-benefits'
+import programBenefits from '@/sections/detail-settlement-programs/program-benefits/constants'
 import {ProgramOverview} from '@/sections/detail-settlement-programs/program-overview'
 import programOverview from '@/sections/detail-settlement-programs/program-overview/constants'
 
@@ -8,6 +10,7 @@ export const DetailSettlementPrograms = () => {
     <>
       <Banner {...bannerSettlementPrograms} />
       <ProgramOverview {...programOverview} />
+      <ProgramBenefits {...programBenefits} />
     </>
   )
 }
