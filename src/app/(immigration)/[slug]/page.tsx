aeff699f24e08fa2a1a6f8ffbf874cfce9354290
Jhuy02 <ttruonghuy2502@gmail.com>
2025-01-10 14:33:28 +0700

    [code]: ??

--- src/app/(immigration)/[slug]/page.tsx
+++ src/app/(immigration)/[slug]/page.tsx
@@ -1,14 +1,22 @@
+import fetchData from '@/fetch/fetchData';
 import fetchDataACF from '@/fetch/fetchDataACF';
 import Immigration from '@/pages/immigration';
+import endpoints from '@/utils/endpoints';
 
 export default async function page({ params }: { params: { slug: string } }) {
-    const [dataAcf] = await Promise.all([
+    const [dataAcf, dataPrograms] = await Promise.all([
         fetchDataACF({
-            api: '/nation?slug='+ params?.slug +'&acf_format=standard',
+            api: '/' + endpoints.taxonomiesSettlement + '?slug=' + params?.slug + '&acf_format=standard',
+            option: {
+                revalidate: 10,
+            },
+        }),
+        fetchData({
+            api: endpoints.settlementPrograms + '?page=1&per_page=8&order=asc&' + endpoints.taxonomiesSettlement + '=' + params?.slug,
             option: {
                 revalidate: 10,
             },
         }),
     ])
-    return <Immigration dataImmigration={dataAcf[0]} />
+    return <Immigration slug={params?.slug} dataImmigration={dataAcf[0]} dataPrograms={dataPrograms} />
 }
