5539ab523c6e85ee1cf4a3911cd9f469f0c87981
SieuBuong <huycloud1999@gmail.com>
2025-01-12 12:56:34 +0700

    Refactor immigration page component for improved readability and consistency in code formatting

--- src/app/(immigration)/[slug]/page.tsx
+++ src/app/(immigration)/[slug]/page.tsx
@@ -1,35 +1,45 @@
-import fetchData from '@/fetch/fetchData';
-import fetchDataACF from '@/fetch/fetchDataACF';
-import Immigration from '@/pages/immigration';
-import endpoints from '@/utils/endpoints';
+import fetchData from '@/fetch/fetchData'
+import fetchDataACF from '@/fetch/fetchDataACF'
+import Immigration from '@/pages/immigration'
+import endpoints from '@/utils/endpoints'
 
-export default async function page({ params }: { params: { slug: string } }) {
-    const [dataAcf, dataPrograms, postRelate] = await Promise.all([
-        fetchDataACF({
-            api: '/' + endpoints.taxonomiesSettlement + '?slug=' + params?.slug + '&acf_format=standard',
-            option: {
-                revalidate: 10,
-            },
-        }),
-        fetchData({
-            api: endpoints.settlementPrograms + '?page=1&per_page=8&order=asc&' + endpoints.taxonomiesSettlement + '=' + params?.slug,
-            option: {
-                revalidate: 10,
-            },
-        }),
-        fetchData({
-            api: '/posts-by-taxonomy?slug=' + params?.slug,
-            option: {
-                revalidate: 10,
-            },
-        }),
-    ])
-    return (
-        <Immigration
-            slug={params?.slug}
-            dataImmigration={dataAcf[0]}
-            dataPrograms={dataPrograms}
-            postRelate={postRelate}
-        />
-    )
+export default async function page({params}: {params: {slug: string}}) {
+  const [dataAcf, dataPrograms, postRelate] = await Promise.all([
+    fetchDataACF({
+      api:
+        '/' +
+        endpoints.taxonomiesSettlement +
+        '?slug=' +
+        params?.slug +
+        '&acf_format=standard',
+      option: {
+        revalidate: 10,
+      },
+    }),
+    fetchData({
+      api:
+        endpoints.settlementPrograms +
+        '?page=1&per_page=8&order=asc&' +
+        endpoints.taxonomiesSettlement +
+        '=' +
+        params?.slug,
+      option: {
+        revalidate: 10,
+      },
+    }),
+    fetchData({
+      api: '/posts-by-taxonomy?slug=' + params?.slug,
+      option: {
+        revalidate: 10,
+      },
+    }),
+  ])
+  return (
+    <Immigration
+      slug={params?.slug}
+      dataImmigration={dataAcf[0]}
+      dataPrograms={dataPrograms}
+      postRelate={postRelate}
+    />
+  )
 }
