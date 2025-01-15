5f3b88fbd892d7a727328aa82f18e9dbc3f4c942
FinnTVD <finnit.th@gmail.com>
2025-01-15 10:02:13 +0700

    UI: responsive

--- src/pages/compare-programs/IndexComparePrograms.tsx
+++ src/pages/compare-programs/IndexComparePrograms.tsx
@@ -2,12 +2,12 @@ import Programs from '@/sections/compare-programs/Programs'
 
 const IndexComparePrograms = () => {
   return (
-    <main>
-      <section className='mt-[6.44rem] h-fit w-full rounded-[2.5rem_2.5rem_0rem_0rem] bg-[#F6F6F4] section-container xsm:!max-w-full xsm:px-0'>
-        <h2 className='text-Phase-1-Brown heading1 xsm:px-[1rem] xsm:font-semibold xsm:tracking-[-0.045rem]'>
+    <main className='bg-[#F6F6F4]'>
+      <section className='mt-[6.44rem] h-fit w-full rounded-[2.5rem_2.5rem_0rem_0rem] section-container xsm:!max-w-full xsm:px-0'>
+        <h2 className='font-optima text-Phase-1-Brown heading1 xsm:px-[1rem] xsm:text-[1.5rem] xsm:font-semibold xsm:leading-[1.3] xsm:tracking-[-0.045rem]'>
           So sánh các chương trình định cư
         </h2>
-        <span className='mb-[4rem] mt-[1rem] block font-normal text-greyscaletext-body body16-r55 xsm:mb-[2rem] xsm:mt-[0.75rem] xsm:px-[1rem]'>
+        <span className='mb-[4rem] mt-[1rem] block font-normal text-greyscaletext-body body16-r55 xsm:mb-[2rem] xsm:mt-[0.75rem] xsm:px-[1rem] xsm:body-14'>
           Chọn chương trình và so sánh giữa các chương trình
         </span>
         <Programs />
--- src/sections/compare-programs/Programs.tsx
+++ src/sections/compare-programs/Programs.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import * as React from 'react'
 import {
   Select,
   SelectContent,
@@ -8,7 +7,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import {cn} from '@/lib/utils'
 import CardPrograms from '@/sections/compare-programs/CardPrograms'
+
+// Import GSAP và ScrollTrigger
+// import {gsap} from 'gsap'
+// import ScrollTrigger from 'gsap/ScrollTrigger'
+// import {useEffect, useState} from 'react'
+
+// gsap.registerPlugin(ScrollTrigger)
 
 const programs = [
   {
@@ -42,25 +49,64 @@ const programs = [
 ]
 
 export default function Programs() {
+  // const [isFixed, setIsFixed] = useState(false)
+
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     const header = document.querySelector('header')
+  //     const table = document.querySelector('#table__programs')
+  //     const taskbar = document.querySelector('#taskbar__sticky')
+
+  //     if (window.innerWidth < 640 && header && table && taskbar) {
+  //       ScrollTrigger.create({
+  //         trigger: table,
+  //         start: `top ${header.getBoundingClientRect().bottom}px`,
+  //         end: `bottom-=50px ${taskbar.getBoundingClientRect().height + header?.getBoundingClientRect().height}px`,
+  //         scrub: true,
+  //         markers: true,
+  //         onEnter: () => {
+  //           setIsFixed(true)
+  //         },
+  //         onEnterBack: () => {
+  //           setIsFixed(true)
+  //         },
+  //         onLeave: () => {
+  //           setIsFixed(false)
+  //         },
+  //         onLeaveBack: () => {
+  //           setIsFixed(false)
+  //         },
+  //       })
+  //     }
+  //   }
+  // }, [])
+
   return (
     <div
       id='table__programs'
       className='hidden-scrollbar relative h-fit w-full px-[1rem] xsm:overflow-x-auto'
     >
       <div className='h-fit w-fit border border-solid'>
-        <div className='sticky right-0 top-0 z-10 flex h-fit w-full'>
-          <div className='flex-1'></div>
-          <div className='flex w-[calc(19.375rem*4)] flex-shrink-0 bg-background'>
+        <div
+          id='taskbar__sticky'
+          className={cn(
+            'relative z-10 flex h-fit w-full transition-all duration-500 sm:sticky sm:right-0 sm:top-[6.4rem]',
+            // isFixed &&
+            //   'overflow-x-auto transition-all duration-500 xsm:fixed xsm:top-[3.72rem]',
+          )}
+        >
+          <div className='flex-1 xsm:w-[6.25rem]'></div>
+          <div className='flex w-[calc(19.375rem*4)] flex-shrink-0 bg-background xsm:w-[calc(13.125rem*4)]'>
             {programs.map((program) => (
               <div
                 key={program.id}
-                className='relative flex h-[5rem] w-[19.375rem] items-center border-l px-[1rem]'
+                className='relative flex h-[5rem] w-[19.375rem] items-center border-l px-[1rem] xsm:h-[4.5rem] xsm:w-[13.125rem] xsm:px-[0.75rem]'
               >
                 <Select>
                   <SelectTrigger className='h-[3rem] w-full rounded-[0.75rem] border-none shadow-[0px_1.203px_4.812px_0px_rgba(0,0,0,0.10)] outline-none ring-0 focus:ring-0'>
                     <SelectValue
                       placeholder={`${program.title} (${program.subtitle})`}
-                      className='body16-m'
+                      className='body16-m xsm:sub-12-m'
                     />
                   </SelectTrigger>
                   <SelectContent className='w-[17.375rem] rounded-[0.5rem] bg-white p-[0.5rem] shadow-[0px_4px_10px_0px_rgba(0,0,0,0.20)]'>
@@ -83,114 +129,114 @@ export default function Programs() {
             ))}
           </div>
         </div>
-        <div className='relative grid h-fit w-full grid-cols-[1fr_19.375rem_19.375rem_19.375rem_19.375rem]'>
+        <div className='relative grid h-fit w-full grid-cols-[1fr_19.375rem_19.375rem_19.375rem_19.375rem] xsm:grid-cols-[6.25rem_13.125rem_13.125rem_13.125rem_13.125rem]'>
           <div></div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[0_1rem_1rem]'
+              className='border-l border-solid p-[0_1rem_1rem] xsm:p-[0_0.75rem_0.75rem]'
             >
               <CardPrograms data={programs?.[0]} />
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Mục đích chương trình
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Visa tạm, sang Mỹ nhanh chóng. Cần duy trì doanh nghiệp để gia hạn
               vĩnh viễn.
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Loại hình đầu tư
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Kinh doanh trực tiếp.
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Mức đầu tư thấp nhất
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               150.000 CAD.
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Thời gian đạt mục tiêu
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Thời gian xét duyệt hồ sơ: 6 tháng.
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Thời gian tối thiểu duy trì khoản đầu tư
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Cần duy trì doanh nghiệp để giữ Visa E-2.
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Thời gian cư trú tối thiểu
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               9 tháng / năm. (để đủ điều kiện lên thường trú)
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Thành viên kèm hồ sơ
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Vợ / chồng hợp pháp. Con cái chưa kết hôn dưới 21 tuổi (Tại thời
               điểm cấp visa). Đảm bảo lý lịch an ninh & sức khỏe (như đương
               đơn).
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Số lần cần sang quốc gia để hoàn tất hồ sơ
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Toàn bộ quy trình làm từ Việt Nam. Sang Mỹ sau khi có thẻ xanh.
             </div>
           ))}
-          <div className='p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE]'>
+          <div className='border-t p-[1.25rem_1rem] text-[1rem] font-semibold leading-normal tracking-[-0.02rem] text-[#121212DE] xsm:p-[0.75rem]'>
             Quyền lợi làm việc
           </div>
           {programs.map((program) => (
             <div
               key={program.id}
-              className='border-l border-solid p-[1.25rem_1rem]'
+              className='border-l border-t border-solid p-[1.25rem_1rem] xsm:p-[0.75rem]'
             >
               Có.
             </div>
