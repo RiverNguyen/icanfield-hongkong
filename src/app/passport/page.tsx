7cc3635339e11f7264e06784a3d88b6782418ada
SieuBuong <huycloud1999@gmail.com>
2025-01-18 11:19:30 +0700

    Enhance Passport page with data fetching and integrate ContactV2 and FormInternationalJourney components for improved user interaction

--- src/app/passport/page.tsx
+++ src/app/passport/page.tsx
@@ -1,7 +1,28 @@
 import IndexPassport from '@/pages/passport/IndexPassport'
-
+import fetchData from '@/fetch/fetchData'
+import fetchDataACF from '@/fetch/fetchDataACF'
 const Page = async () => {
-  return <IndexPassport />
+  const requestTaxonomies = {
+    api: '/taxonomies-settlement',
+    option: {
+      revalidate: 10,
+    },
+  }
+  const [dataTaxonomies, dataAcf] = await Promise.all([
+    fetchData(requestTaxonomies),
+    fetchDataACF({
+      api: '/pages/1632?_fields=acf&acf_format=standard',
+      option: {
+        revalidate: 10,
+      },
+    }),
+  ])
+  return (
+    <IndexPassport
+      dataAcf={dataAcf.acf}
+      dataNationSettlement={dataTaxonomies?.nation}
+    />
+  )
 }
 
 export default Page
