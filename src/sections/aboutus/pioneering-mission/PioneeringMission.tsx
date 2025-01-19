102ae159d2776e15ae8ab010f2c082115c6b84fd
SieuBuong <huycloud1999@gmail.com>
2025-01-19 23:50:12 +0700

    Refactor office map components for improved data handling and structure; add styles for office map section

--- src/sections/aboutus/pioneering-mission/PioneeringMission.tsx
+++ src/sections/aboutus/pioneering-mission/PioneeringMission.tsx
@@ -38,25 +38,25 @@ export default function PioneeringMission({
         </div>
         <div className='sticky top-[3rem] h-[43.75rem] flex-1 space-y-[2rem] p-[6.5rem_5rem_6.5rem_3.5rem] xsm:top-[3.75rem] xsm:hidden xsm:p-[4rem_1rem_1.5rem_1rem]'>
           <h2
-            dangerouslySetInnerHTML={{__html: dataAcfPioneeringMission?.title}}
+            dangerouslySetInnerHTML={{__html: dataAcfPioneeringMission?.title || ''}}
             className='font-optima font-semibold text-brown heading1'
           ></h2>
           <div
             dangerouslySetInnerHTML={{
-              __html: dataAcfPioneeringMission?.decscripts,
+              __html: dataAcfPioneeringMission?.decscripts || '',
             }}
             className='space-y-[0.75rem] [&_p]:tracking-[-0.02rem] [&_p]:text-greyscaletext-400 [&_p]:body16'
           ></div>
         </div>
         <div
           dangerouslySetInnerHTML={{
-            __html: dataAcfPioneeringMission?.decscripts,
+            __html: dataAcfPioneeringMission?.decscripts || '',
           }}
           className='space-y-[1rem] px-[1rem] sm:hidden [&_p]:tracking-[-0.00875rem] [&_p]:text-greyscaletext-body [&_p]:body-14'
         ></div>
         <div className='sticky top-0 z-10 flex-1 bg-background p-[6.5rem_7.5rem_6.5rem_3.5rem] sm:hidden xsm:top-[3.75rem] xsm:p-[1rem_1rem_1.5rem_1rem]'>
           <h2
-            dangerouslySetInnerHTML={{__html: dataAcfPioneeringMission?.title}}
+            dangerouslySetInnerHTML={{__html: dataAcfPioneeringMission?.title || ''}}
             className='font-optima text-brown heading1'
           ></h2>
         </div>
