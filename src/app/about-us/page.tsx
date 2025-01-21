32b0f4e869885f688492cf25dc2c03a5b9fa4e09
Jhuy02 <ttruonghuy2502@gmail.com>
2025-01-21 09:58:14 +0700

    [code]: add filter

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,22 +1,13 @@
-import fetchData from '@/fetch/fetchData'
 import fetchDataACF from '@/fetch/fetchDataACF'
 import IndexAboutUs from '@/pages/about-us/IndexAboutUs'
-import endpoints from '@/utils/endpoints'
 const page = async () => {
-  const HomepageMap = {
-    api: endpoints.homepageMap,
-    option: {
-      revalidate: 10,
-    },
-  }
-  const [dataAcf, dataMap] = await Promise.all([
+  const [dataAcf] = await Promise.all([
     fetchDataACF({
       api: '/pages/101?acf_format=standard',
       option: {
         revalidate: 10,
       },
     }),
-    fetchData(HomepageMap),
   ])
   return (
     <IndexAboutUs
