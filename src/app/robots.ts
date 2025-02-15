1b62991c6bfc81e10ccd8320cf9164471383947c
SieuBuong <huycloud1999@gmail.com>
2025-02-15 17:29:34 +0700

    fix: update robots rules to disallow indexing of all pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ export default function robots() {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      disallow: '/', // Không cho phép lập chỉ mục bất kỳ trang nào
     },
     sitemap: `${process.env.NEXT_PUBLIC_DOMAIN}/sitemap.xml`,
   }
