d6b275d50660829abe1bb5be9f2b5dd416e4cde9
SieuBuong <huycloud1999@gmail.com>
2025-03-12 17:17:19 +0700

    feat: implement sitemap generation and remove obsolete sitemap.xml file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,62 @@
+import fetchData from '@/fetch/fetchData'
+
+export default async function sitemap() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  const locales = ['', '/en', '/es']
+  const lastModified = new Date()
+
+  // Fetch dữ liệu với delay nhẹ để tránh ảnh hưởng TBT
+  const slugPosst = await fetchData({
+    api: '/slugs?post_type=post',
+  })
+  const slugSettlement = await fetchData({
+    api: '/slugs?post_type=settlement-program',
+  })
+  // Các trang tĩnh
+  const fixedPages = locales.flatMap((locale) =>
+    [
+      '',
+      '/ti-tuc',
+      '/dinh-cu-canada',
+      '/dinh-cu-uc',
+      '/dinh-cu-my',
+      '/dinh-cu-chau-au',
+      '/dinh-cu-caribe',
+      '/ve-chung-toi',
+      '/lien-he',
+      '/bat-dong-san-uc',
+      '/EB5',
+      '/tham-dinh-ho-so',
+      '/ho-chieu',
+      '/so-sanh-chuong-trinh',
+    ].map((path) => ({
+      url: `${baseUrl}${locale}${path}`,
+      lastModified,
+      priority: path === '' ? 1 : 0.9,
+    })),
+  )
+
+  // Các trang động
+  const dynamicPages = []
+  if (slugPosst.length > 0) {
+    dynamicPages.push(
+      ...slugPosst.map((post: string) => ({
+        url: `${baseUrl}/tin-tuc/${post}`,
+        lastModified,
+        priority: 0.8,
+      })),
+    )
+  }
+
+  if (slugSettlement.length > 0) {
+    dynamicPages.push(
+      ...slugSettlement.map((settlement: string) => ({
+        url: `${baseUrl}/chuong-trinh-dinh-cu/${settlement}`,
+        lastModified,
+        priority: 0.8,
+      })),
+    )
+  }
+
+  return [...fixedPages, ...dynamicPages]
+}
