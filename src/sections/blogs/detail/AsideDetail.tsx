fdd70f1e21e3ab3307f22a092c636026ba953aab
SieuBuong <huycloud1999@gmail.com>
2025-05-12 11:09:40 +0700

    fix: enhance heading handling in ContentDetail and IndexAside components; improve formatting and accessibility

--- src/sections/blogs/detail/AsideDetail.tsx
+++ src/sections/blogs/detail/AsideDetail.tsx
@@ -41,7 +41,13 @@ export default function AsideDetail({
           {title}
         </h1>
       )}
-      <div className='sm:sticky sm:top-[8.4375rem] sm:pb-[1rem]'>
+      <div
+        style={{
+          scrollbarWidth: 'thin',
+          scrollbarColor: '#ad6903 transparent',
+        }}
+        className='h-[33rem] overflow-y-scroll sm:sticky sm:top-[8.4375rem] sm:pb-[1rem]'
+      >
         <div className='mb-[1.5rem] rounded-[0.75rem] border-[2px] border-solid border-[#eee] bg-[#F6F5F3] p-[1.5rem] xsm:rounded-[0.625rem] xsm:border-[0.4px] xsm:border-[rgba(0,0,0,0.10)] xsm:p-[0.94rem_1rem]'>
           <div className='mb-[1.62rem] w-full xsm:mb-[0.75rem] xsm:flex xsm:items-center xsm:justify-between'>
             <p className='text-[1.25rem] font-semibold tracking-[-0.0125rem] text-orangetext-900 xsm:text-[0.875rem] xsm:tracking-[-0.00875rem]'>
--- src/sections/blogs/detail/ContentDetail.tsx
+++ src/sections/blogs/detail/ContentDetail.tsx
@@ -3,13 +3,14 @@
 import Share from '@/components/share/Share'
 import useIsMobile from '@/hooks/useIsMobile'
 
-const addIdsToH2Tags = (htmlString: string) => {
+const addIdsToHeadings = (htmlString: string) => {
   let index = 1
   if (!htmlString) return ''
-  return htmlString.replace(/<h2[^>]*>/g, (match) => {
+  return htmlString.replace(/<(h2|h3)[^>]*>/g, (match, tag:any) => { //eslint-disable-line
     return `${match.slice(0, -1)} id="section-${index++}">`
   })
 }
+
 export default function ContentDetail({
   dataContent,
   author,
@@ -20,10 +21,10 @@ export default function ContentDetail({
   title: string
 }) {
   const isMobile = useIsMobile()
-  const htmlWithIds = addIdsToH2Tags(dataContent)
+  const htmlWithIds = addIdsToHeadings(dataContent)
   const cleanedHtml = htmlWithIds
-    ?.replace(/\[caption.*?\]/g, '<div class="wp-caption">') // Bọc nội dung trong <div>
-    .replace(/\[\/caption\]/g, '</div>') // Đóng thẻ
+    ?.replace(/\[caption.*?\]/g, '<div class="wp-caption">')
+    .replace(/\[\/caption\]/g, '</div>')
 
   return (
     <div className='w-[58.3125rem] xsm:w-full'>
@@ -34,7 +35,7 @@ export default function ContentDetail({
       )}
       <div
         className='[&_h2]:content-h2 [&_img]:content-img [&_.wp-caption]:text-gray-500 [&_.wp-caption]:italic [&_.wp-caption]:text-center  [&_p]:content-p [&_span]:content-span [&_ul]:content-ul [&_ul_li]:content-ul--li [&_strong]:content-strong [&_ol]:content-ol [&_ol_li]:content-ol--li flex-1 [&_a]:text-blue-600 [&_strong]:font-bold'
-        dangerouslySetInnerHTML={{__html: cleanedHtml || ''}}
+        dangerouslySetInnerHTML={{ __html: cleanedHtml || '' }}
       ></div>
       <div className='my-[1.5rem] h-[0.0625rem] w-full bg-[rgba(0,0,0,0.04)]'></div>
       <div className='flex w-full items-center justify-between'>
