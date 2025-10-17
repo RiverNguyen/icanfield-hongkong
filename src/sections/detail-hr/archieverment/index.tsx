154dd23e11d72fb6b91b44bc44c1af69014e8b19
duc82 <duccdht123@gmail.com>
2025-10-17 15:55:34 +0700

    [fix]:update archieverment

--- src/sections/detail-hr/archieverment/index.tsx
+++ src/sections/detail-hr/archieverment/index.tsx
@@ -159,8 +159,7 @@ export default function Achievements() {
               <div className='flex items-center gap-[2rem] self-stretch xsm:flex-col xsm:items-start xsm:gap-[1.75rem] xsm:self-stretch'>
                 {group.map((item, itemIndex) => (
                   <Fragment key={`item-${slideIndex}-${itemIndex}`}>
-                    {/* p-[1.5rem] */}
-                    <div className='flex min-h-[13.4375rem] shrink-0 flex-grow basis-0 flex-col items-center gap-[1.25rem] rounded-[1rem] py-[1.5rem] xsm:min-h-full xsm:justify-center xsm:self-stretch xsm:p-0'>
+                    <div className='flex h-[13.4375rem] shrink-0 flex-grow basis-0 flex-col items-center gap-[1.25rem] rounded-[1rem] py-[1.5rem] xsm:h-full xsm:justify-center xsm:self-stretch xsm:p-0'>
                       <div className='flex flex-col items-center gap-[1.25rem] xsm:gap-[0.75rem] xsm:self-stretch'>
                         <Image
                           src={item.imgSrc}
