e2abac500eefee3fc6dea6f81afbc9a825ddef93
Mr Lee <proxuthanh99yd@gmail.com>
2026-01-31 11:19:31 +0700

    update

--- src/sections/passport/research/SearchPassport.tsx
+++ src/sections/passport/research/SearchPassport.tsx
@@ -1,177 +1,179 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Check, ChevronsUpDown } from 'lucide-react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {Check, ChevronsUpDown} from 'lucide-react'
+import {useForm} from 'react-hook-form'
+import {z} from 'zod'
 
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import {cn} from '@/lib/utils'
+import {Button} from '@/components/ui/button'
 import {
-	Command,
-	CommandEmpty,
-	CommandGroup,
-	CommandInput,
-	CommandItem,
-	CommandList,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
 } from '@/components/ui/command'
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from '@/components/ui/form'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import {Popover, PopoverContent, PopoverTrigger} from '@/components/ui/popover'
 
 import countryList from '@/sections/passport/research/countrylist.json'
 import countryListZh from '@/sections/passport/research/countrylist_zh_full.json'
 import countryListZhCn from '@/sections/passport/research/countrylist_zh_cn_full.json'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import {usePathname, useRouter, useSearchParams} from 'next/navigation'
+import {useEffect, useState} from 'react'
 import ICSearch from '@/components/icon/ICSearch'
-import { useLocale, useTranslations } from 'next-intl'
-
-const FormSchema = z.object({
-	postal: z.string({
-		required_error: t('vui_long_chon_quoc_gia'),
-	}),
-})
+import {useLocale, useTranslations} from 'next-intl'
 
 interface IProps {
-	setCodePostal: React.Dispatch<React.SetStateAction<string | null>>
+  setCodePostal: React.Dispatch<React.SetStateAction<string | null>>
 }
 
-const SearchPassport = ({ setCodePostal }: IProps) => {
-	const t = useTranslations('')
-	const locale = useLocale()
-	const router = useRouter()
-	const pathName = usePathname()
-	const searchParams = useSearchParams()
+const SearchPassport = ({setCodePostal}: IProps) => {
+  const t = useTranslations('')
+  const locale = useLocale()
+  const router = useRouter()
+  const pathName = usePathname()
+  const searchParams = useSearchParams()
+  const [open, setOpen] = useState(false)
 
-	const [open, setOpen] = useState(false)
+  const FormSchema = z.object({
+    postal: z.string({
+      required_error: t('vui_long_chon_quoc_gia'),
+    }),
+  })
 
-	const form = useForm<z.infer<typeof FormSchema>>({
-		resolver: zodResolver(FormSchema),
-	})
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
 
-	useEffect(() => {
-		const postal = searchParams?.get('postal')
-		if (postal) {
-			form.setValue('postal', postal)
-		}
-	}, [searchParams])
+  useEffect(() => {
+    const postal = searchParams?.get('postal')
+    if (postal) {
+      form.setValue('postal', postal)
+    }
+  }, [searchParams])
 
-	function onSubmit(data: z.infer<typeof FormSchema>) {
-		setCodePostal(data.postal)
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    setCodePostal(data.postal)
 
-		const paramNew = new URLSearchParams(searchParams ?? '')
+    const paramNew = new URLSearchParams(searchParams ?? '')
 
-		paramNew.set('postal', data.postal)
-		router.push(pathName + '?' + paramNew.toString(), {
-			scroll: false,
-		})
-	}
+    paramNew.set('postal', data.postal)
+    router.push(pathName + '?' + paramNew.toString(), {
+      scroll: false,
+    })
+  }
 
-	const countries = locale === 'zh' ? countryListZh : locale === 'zh-cn' ? countryListZhCn : countryList
+  const countries =
+    locale === 'zh'
+      ? countryListZh
+      : locale === 'zh-cn'
+        ? countryListZhCn
+        : countryList
 
-	function handleFindCountry(postal: string) {
-		const countryCurrent = countries?.find(
-			(item: any) => item.code === postal,
-		)
-		return countryCurrent?.name || t('khong_tim_thay')
-	}
+  function handleFindCountry(postal: string) {
+    const countryCurrent = countries?.find((item: any) => item.code === postal)
+    return countryCurrent?.name || t('khong_tim_thay')
+  }
 
-	return (
-		<div className='rounded-[1.25rem] bg-white p-[1.5rem] shadow-[0px_4px_24px_0px_rgba(0,0,0,0.04)] xsm:p-[1rem]'>
-			<h2 className='font-medium text-Phase-1-Brown heading3 xsm:text-[1.25rem] xsm:leading-[1.3] xsm:tracking-[-0.025rem] font-optima'>
-				{t('tra_cuu_thong_tin_ho_chieu')}
-			</h2>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className='flex items-end sm:space-x-[0.5rem] xsm:flex-col xsm:space-y-[0.75rem]'
-				>
-					<FormField
-						control={form.control}
-						name='postal'
-						render={({ field }) => (
-							<FormItem className='flex w-full flex-col space-y-0'>
-								<FormLabel className='mb-[0.75rem] mt-[1.5rem] inline-block font-optima text-tagtext body-14-m xsm:my-[1rem] xsm:text-[-0.0175rem]'>
-									{t('quoc_gia_ho_chieu')}
-								</FormLabel>
-								<Popover
-									open={open}
-									onOpenChange={setOpen}
-								>
-									<PopoverTrigger asChild>
-										<FormControl>
-											<Button
-												variant='outline'
-												role='combobox'
-												className={cn(
-													'h-[3rem] w-[17.6875rem] justify-between rounded-[0.5rem] border-none bg-[#F6F6F6] outline-none xsm:h-[2.75rem] xsm:w-full',
-													!field.value && 'text-muted-foreground',
-												)}
-											>
-												{field.value
-													? handleFindCountry(field.value)
-													: t('chon_quoc_gia')}
-												<ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
-											</Button>
-										</FormControl>
-									</PopoverTrigger>
-									<PopoverContent className='w-[17.6875rem] bg-white p-0 xsm:w-[calc(100vw-4.2rem)]'>
-										<Command>
-											<CommandInput placeholder={t('tim_kiem_quoc_gia')} />
-											<CommandList>
-												<CommandEmpty>{t('khong_tim_thay')}</CommandEmpty>
-												<CommandGroup>
-													{countries?.map((country: any) => (
-														<CommandItem
-															className='cursor-pointer lg:hover:bg-greyscaletext-50'
-															value={country?.code}
-															key={country?.name}
-															onSelect={() => {
-																form.setValue('postal', country?.code)
-																setOpen(false)
-															}}
-														>
-															{country?.name}
-															<Check
-																className={cn(
-																	'ml-auto',
-																	country?.code === field.value
-																		? 'opacity-100'
-																		: 'opacity-0',
-																)}
-															/>
-														</CommandItem>
-													))}
-												</CommandGroup>
-											</CommandList>
-										</Command>
-									</PopoverContent>
-								</Popover>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<button
-						type='submit'
-						className='flex h-[3rem] flex-1 items-center justify-between rounded-[0.5rem] bg-[linear-gradient(95deg,#95502F_-4.54%,#F5C178_95.42%)] px-[1.5rem] xsm:h-[2.75rem] xsm:min-h-[2.75rem] xsm:w-full xsm:justify-center xsm:space-x-[0.62rem]'
-					>
-						<ICSearch className='size-[1.25rem] [&>path]:stroke-white' />
-						<span className='whitespace-nowrap text-[0.875rem] font-medium leading-normal text-white'>
-							{t('tra_cuu')}
-						</span>
-					</button>
-				</form>
-			</Form>
-		</div>
-	)
+  return (
+    <div className='rounded-[1.25rem] bg-white p-[1.5rem] shadow-[0px_4px_24px_0px_rgba(0,0,0,0.04)] xsm:p-[1rem]'>
+      <h2 className='font-optima font-medium text-Phase-1-Brown heading3 xsm:text-[1.25rem] xsm:leading-[1.3] xsm:tracking-[-0.025rem]'>
+        {t('tra_cuu_thong_tin_ho_chieu')}
+      </h2>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex items-end sm:space-x-[0.5rem] xsm:flex-col xsm:space-y-[0.75rem]'
+        >
+          <FormField
+            control={form.control}
+            name='postal'
+            render={({field}) => (
+              <FormItem className='flex w-full flex-col space-y-0'>
+                <FormLabel className='mb-[0.75rem] mt-[1.5rem] inline-block font-optima text-tagtext body-14-m xsm:my-[1rem] xsm:text-[-0.0175rem]'>
+                  {t('quoc_gia_ho_chieu')}
+                </FormLabel>
+                <Popover
+                  open={open}
+                  onOpenChange={setOpen}
+                >
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant='outline'
+                        role='combobox'
+                        className={cn(
+                          'h-[3rem] w-[17.6875rem] justify-between rounded-[0.5rem] border-none bg-[#F6F6F6] outline-none xsm:h-[2.75rem] xsm:w-full',
+                          !field.value && 'text-muted-foreground',
+                        )}
+                      >
+                        {field.value
+                          ? handleFindCountry(field.value)
+                          : t('chon_quoc_gia')}
+                        <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className='w-[17.6875rem] bg-white p-0 xsm:w-[calc(100vw-4.2rem)]'>
+                    <Command>
+                      <CommandInput placeholder={t('tim_kiem_quoc_gia')} />
+                      <CommandList>
+                        <CommandEmpty>{t('khong_tim_thay')}</CommandEmpty>
+                        <CommandGroup>
+                          {countries?.map((country: any) => (
+                            <CommandItem
+                              className='cursor-pointer lg:hover:bg-greyscaletext-50'
+                              value={country?.code}
+                              key={country?.name}
+                              onSelect={() => {
+                                form.setValue('postal', country?.code)
+                                setOpen(false)
+                              }}
+                            >
+                              {country?.name}
+                              <Check
+                                className={cn(
+                                  'ml-auto',
+                                  country?.code === field.value
+                                    ? 'opacity-100'
+                                    : 'opacity-0',
+                                )}
+                              />
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <button
+            type='submit'
+            className='flex h-[3rem] flex-1 items-center justify-between rounded-[0.5rem] bg-[linear-gradient(95deg,#95502F_-4.54%,#F5C178_95.42%)] px-[1.5rem] xsm:h-[2.75rem] xsm:min-h-[2.75rem] xsm:w-full xsm:justify-center xsm:space-x-[0.62rem]'
+          >
+            <ICSearch className='size-[1.25rem] [&>path]:stroke-white' />
+            <span className='whitespace-nowrap text-[0.875rem] font-medium leading-normal text-white'>
+              {t('tra_cuu')}
+            </span>
+          </button>
+        </form>
+      </Form>
+    </div>
+  )
 }
 
 export default SearchPassport
--- src/views/homepage/index.tsx
+++ src/views/homepage/index.tsx
@@ -1,151 +1,158 @@
 'use client'
-import { useTranslations } from 'next-intl'
-import BannerHomepage, { IBannerHomepageProps } from '@/sections/homepage/banner'
+import {useTranslations} from 'next-intl'
+import BannerHomepage, {IBannerHomepageProps} from '@/sections/homepage/banner'
 import {
-	IItemInvestmentOpportunities,
-	InvestmentOpportunities,
+  IItemInvestmentOpportunities,
+  InvestmentOpportunities,
 } from '@/sections/homepage/investment-opportunities'
-import { ItemNewsFeatured, ItemNewsHP } from '@/sections/homepage/news-homepage'
-import { IItemProudJourney } from '@/sections/homepage/proud-journey'
-import { Media } from '@/types/image.interface'
+import {ItemNewsFeatured, ItemNewsHP} from '@/sections/homepage/news-homepage'
+import {IItemProudJourney} from '@/sections/homepage/proud-journey'
+import {Media} from '@/types/image.interface'
 import dynamic from 'next/dynamic'
-import { DataMapHomepage } from '@/sections/homepage/map-discover/dataMap.interface'
-import { FilterData } from '@/sections/homepage/banner/bannerHp.interface'
-const GlobalImmigration = dynamic(
-	() => import('@/sections/homepage/global-immigration'),
-	{ ssr: false },
-)
-const NewsFlow = dynamic(() => import('@/sections/homepage/news-homepage'), {
-	ssr: false,
-	loading: () => <p>{t('dang_tai_tin_tuc')}</p>,
-})
-const ProudJourney = dynamic(
-	() => import('@/sections/homepage/proud-journey'),
-	{
-		ssr: false,
-		loading: () => <p>{t('dang_tai_hanh_trinh')}</p>,
-	},
-)
-const MapDiscover = dynamic(() => import('@/sections/homepage/map-discover'), {
-	ssr: false,
-	loading: () => <p>{t('dang_tai_ban_do')}</p>,
-})
-const TalentedTeam = dynamic(
-	() => import('@/sections/homepage/talented-team'),
-	{
-		ssr: false,
-		loading: () => <p>{t('dang_tai_doi_ngu')}</p>,
-	},
-)
-const FormHomepage = dynamic(
-	() => import('@/sections/homepage/form-homepage'),
-	{
-		ssr: false,
-		loading: () => <p>{t('dang_tai_bieu_mau')}</p>,
-	},
-)
+import {DataMapHomepage} from '@/sections/homepage/map-discover/dataMap.interface'
+import {FilterData} from '@/sections/homepage/banner/bannerHp.interface'
+
 interface HomeData {
-	acf: {
-		home_banner?: IBannerHomepageProps & {
-			type: 'upload' | 'youtube' | 'tiktok' | 'slide'
-		}
-		home_global_immigration?: {
-			title: string
-			description: string
-			count_number: {
-				number: string
-				label: string
-				title: string
-			}[]
-		}
-		home_map_discover?: {
-			logo: Media
-			slogan: string
-			title: string
-			description: string
-		}
-		investment_opportunities?: {
-			title: string
-			subtitle: string
-			items: IItemInvestmentOpportunities[]
-		}
-		home_talented_team?: {
-			title: string
-			subtitle: string
-			talented_team: {
-				image: Media
-				name: string
-				position: string
-				link: string
-			}[]
-		}
-		home_proud_journey?: {
-			title: string
-			proud_journey: IItemProudJourney[]
-		}
-	}
+  acf: {
+    home_banner?: IBannerHomepageProps & {
+      type: 'upload' | 'youtube' | 'tiktok' | 'slide'
+    }
+    home_global_immigration?: {
+      title: string
+      description: string
+      count_number: {
+        number: string
+        label: string
+        title: string
+      }[]
+    }
+    home_map_discover?: {
+      logo: Media
+      slogan: string
+      title: string
+      description: string
+    }
+    investment_opportunities?: {
+      title: string
+      subtitle: string
+      items: IItemInvestmentOpportunities[]
+    }
+    home_talented_team?: {
+      title: string
+      subtitle: string
+      talented_team: {
+        image: Media
+        name: string
+        position: string
+        link: string
+      }[]
+    }
+    home_proud_journey?: {
+      title: string
+      proud_journey: IItemProudJourney[]
+    }
+  }
 }
 interface NewsData {
-	title: string
-	news_flow: ItemNewsFeatured[]
-	news: {
-		id: number
-		title_categories: string
-		name: string
-		slug: string
-		description: string
-		posts: ItemNewsHP[]
-	}[]
+  title: string
+  news_flow: ItemNewsFeatured[]
+  news: {
+    id: number
+    title_categories: string
+    name: string
+    slug: string
+    description: string
+    posts: ItemNewsHP[]
+  }[]
 }
 const HomePage = ({
-	homeData,
-	newsData,
-	dataFilter,
-	dataMap,
+  homeData,
+  newsData,
+  dataFilter,
+  dataMap,
 }: {
-	homeData: HomeData
-	newsData: NewsData
-	dataFilter: FilterData
-	dataMap: DataMapHomepage
+  homeData: HomeData
+  newsData: NewsData
+  dataFilter: FilterData
+  dataMap: DataMapHomepage
 }) => {
-	const t = useTranslations()
-	const {
-		home_banner,
-		home_global_immigration,
-		home_map_discover,
-		investment_opportunities,
-		home_talented_team,
-		home_proud_journey,
-	} = homeData?.acf || {}
+  const t = useTranslations()
 
-	return (
-		<main className='bg-background'>
-			{home_banner && (
-				<BannerHomepage
-					data={home_banner}
-					dataFilter={dataFilter}
-				/>
-			)}
-			{home_global_immigration && (
-				<GlobalImmigration data={home_global_immigration} />
-			)}
-			{home_map_discover && (
-				<MapDiscover
-					data={home_map_discover}
-					dataMap={dataMap}
-				/>
-			)}
-			{investment_opportunities && (
-				<InvestmentOpportunities data={investment_opportunities} />
-			)}
-			{home_talented_team && <TalentedTeam data={home_talented_team} />}
-			{home_proud_journey && <ProudJourney data={home_proud_journey} />}
-			<FormHomepage />
-			{newsData && (newsData.title || newsData.news_flow?.length || newsData.news?.length) && (
-				<NewsFlow data={newsData} />
-			)}
-		</main>
-	)
+  const GlobalImmigration = dynamic(
+    () => import('@/sections/homepage/global-immigration'),
+    {ssr: false},
+  )
+  const NewsFlow = dynamic(() => import('@/sections/homepage/news-homepage'), {
+    ssr: false,
+    loading: () => <p>{t('dang_tai_tin_tuc')}</p>,
+  })
+  const ProudJourney = dynamic(
+    () => import('@/sections/homepage/proud-journey'),
+    {
+      ssr: false,
+      loading: () => <p>{t('dang_tai_hanh_trinh')}</p>,
+    },
+  )
+  const MapDiscover = dynamic(
+    () => import('@/sections/homepage/map-discover'),
+    {
+      ssr: false,
+      loading: () => <p>{t('dang_tai_ban_do')}</p>,
+    },
+  )
+  const TalentedTeam = dynamic(
+    () => import('@/sections/homepage/talented-team'),
+    {
+      ssr: false,
+      loading: () => <p>{t('dang_tai_doi_ngu')}</p>,
+    },
+  )
+  const FormHomepage = dynamic(
+    () => import('@/sections/homepage/form-homepage'),
+    {
+      ssr: false,
+      loading: () => <p>{t('dang_tai_bieu_mau')}</p>,
+    },
+  )
+
+  const {
+    home_banner,
+    home_global_immigration,
+    home_map_discover,
+    investment_opportunities,
+    home_talented_team,
+    home_proud_journey,
+  } = homeData?.acf || {}
+
+  return (
+    <main className='bg-background'>
+      {home_banner && (
+        <BannerHomepage
+          data={home_banner}
+          dataFilter={dataFilter}
+        />
+      )}
+      {home_global_immigration && (
+        <GlobalImmigration data={home_global_immigration} />
+      )}
+      {home_map_discover && (
+        <MapDiscover
+          data={home_map_discover}
+          dataMap={dataMap}
+        />
+      )}
+      {investment_opportunities && (
+        <InvestmentOpportunities data={investment_opportunities} />
+      )}
+      {home_talented_team && <TalentedTeam data={home_talented_team} />}
+      {home_proud_journey && <ProudJourney data={home_proud_journey} />}
+      <FormHomepage />
+      {newsData &&
+        (newsData.title ||
+          newsData.news_flow?.length ||
+          newsData.news?.length) && <NewsFlow data={newsData} />}
+    </main>
+  )
 }
 
 export default HomePage
